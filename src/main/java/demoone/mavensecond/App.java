d0e6b5c2431a80fdc058d7590332f80c478d98a0
rk8663 <rkchari002@gmail.com>
2022-03-05 09:06:06 +0530

    i am commiting test file for requirement

---
+++ src/main/java/demoone/mavensecond/App.java
@@ -0,0 +1,13 @@
+package demoone.mavensecond;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/demoone/mavensecond/Demo.java
@@ -0,0 +1,12 @@
+package demoone.mavensecond;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void hello() {
+		System.out.println("hello()");
+	}
+	
+}
